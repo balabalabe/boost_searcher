dffde9791e019abe7c16444963875079206dfd6a
balabalabe <43670168+balabalabe@users.noreply.github.com>
2020-09-28 21:21:39 +0800

    Update Util.hpp

--- common/Util.hpp
+++ common/Util.hpp
@@ -1,32 +1,34 @@
 #pragma once
 
+#include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <boost/algorithm/string.hpp>
 
+using namespace std;
 namespace common {
 
 class Util {
 public:
-  static bool read(const std::string& input, std::string* output) {
-    std::ifstream file(input.c_str(), std::ios::binary);
-    if (!file.is_open()) {
-      return false;
+  static bool read(const string& input, string* output) {
+    ifstream file(input_path.c_str());
+    if(!file.is_open()){
+       return false;
     }
-    file.seekg(0, file.end);
-    int length = file.tellg();
-    file.seekg(0, file.beg);
-    output->resize(length);
-    file.read(const_cast<char*>(output->data()), length);
+    
+    string line;
+    while(getline(file,line)){
+       *output += (line+"\n");
+    }
     file.close();
     return true;
   }
-
-  static void split(const std::string& input, const std::string& delimiter, std::vector<std::string>* output) {
+  
+  static void split(const string& input, const string& delimiter, vector<std::string>* output) {
     boost::split(*output, input, boost::is_any_of(delimiter), boost::token_compress_off);
   }
 };
 
-} 
+}
 
